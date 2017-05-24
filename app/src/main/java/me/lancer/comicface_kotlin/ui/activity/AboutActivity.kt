9b6e230324cc02fcadef34ee5440b0715818aa82
HuangFangzhi <huangfangzhi@icloud.com>
2017-05-24 15:47:04 +0800

    WebView.

--- app/src/main/java/me/lancer/comicface_kotlin/ui/activity/AboutActivity.kt
+++ app/src/main/java/me/lancer/comicface_kotlin/ui/activity/AboutActivity.kt
@@ -7,6 +7,11 @@ import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import me.lancer.comicface_kotlin.R
 import kotlinx.android.synthetic.main.activity_about.*
+import android.webkit.WebResourceRequest
+import android.webkit.WebViewClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+
 
 class AboutActivity : AppCompatActivity() {
 
@@ -16,6 +21,20 @@ class AboutActivity : AppCompatActivity() {
         collapsingToolbar.title = getString(R.string.about_name)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        webView.getSettings().setSupportZoom(true)
+        webView.getSettings().setBuiltInZoomControls(true)
+        webView.getSettings().setUseWideViewPort(true)
+        webView.getSettings().setJavaScriptEnabled(true)
+        webView.getSettings().setLoadWithOverviewMode(true)
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN)
+        webView.getSettings().setLoadWithOverviewMode(true)
+        webView.setWebViewClient(object : WebViewClient() {
+
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                return false
+            }
+
+        })
         webView.loadUrl("https://github.com/1anc3r")
     }
 
